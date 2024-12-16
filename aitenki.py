452e988ced1fb93c059b5645d15ab0c71489d18f
kenji yamazaki <kenzisggkenzisgg@gmail.com>
2024-12-17 01:45:46 +0900

    no message

--- aitenki.py
+++ aitenki.py
@@ -1,10 +1,9 @@
 import streamlit as st
-import requests # リクエストするための機能をインポート
-from datetime import datetime # 現在時刻などの時間を扱う機能をインポート
-import pandas as pd # データフレームを扱う機能をインポート
+import requests
+from datetime import datetime
+import pandas as pd
 
 st.write("天気アプリ")
-
 
 # 選択肢を作成
 city_code_list = {
@@ -15,40 +14,77 @@ city_code_list = {
 city_code_index = st.selectbox("地域を選んでください。", options=list(city_code_list.keys()), index=0)
 city_code = city_code_list[city_code_index] # 選択したキーからAPIのリクエストに使うcityコードに変換し、city_codeに代入
 
-url = "https://weather.tsukumijima.net/api/forecast/city/" + city_code # APIにリクエストするURLを作成
+city_code = city_code_list[city_code_index]  # 選択したキーからAPIのリクエストに使うcityコードに変換し、city_codeに代入
+
+url = "https://weather.tsukumijima.net/api/forecast/city/" + city_code  # APIにリクエストするURLを作成
+
+response = requests.get(url)  # 作成したリクエスト用URLでアクセスして、responseに代入
+
+weather_json = response.json()  # responseにjson形式の天気のデータが返ってくるので、response.json()をweather_jsonに代入
+now_hour = datetime.now().hour  # 現在の天気情報取得のために、現在時刻の時間をnow_hourに代入
 
 
-response = requests.get(url) # 作成したリクエスト用URLでアクセスして、responseに代入
-
-weather_json = response.json() # responseにjson形式の天気のデータが返ってくるので、response.json()をweather_jsonに代入
-now_hour = datetime.now().hour # 現在の天気情報取得のために、現在時刻の時間をnow_hourに代入
-
-# 今日の天気はweather_json['forecasts'][0]['chanceOfRain']
-# 明日の天気はweather_json['forecasts'][1]['chanceOfRain']
-# 明後日の天気はweather_json['forecasts'][2]['chanceOfRain']
-# にそれぞれ格納されている
-
-# 天気の情報を0-6時、6-12時、12-18時、18-24時の4つに分けて降水確率を今日、明日、明後日の３日間の天気を返すため、場合分けする。
-if 0 <= now_hour and now_hour < 6:
-    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T00_06'] # 今日の0-6時の降水確率を取得し、weather_nowに代入
-elif 6 <= now_hour and now_hour < 12:
-    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T06_12'] # 今日の6-12時の降水確率を取得し、weather_nowに代入
-elif 12 <= now_hour and now_hour < 18:
-    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T12_18'] # 今日の12-18時の降水確率を取得し、weather_nowに代入
+# 天気の情報を0-6時、6-12時、12-18時、18-24時の4つに分けて降水確率を取得
+if 0 <= now_hour < 6:
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T00_06']
+elif 6 <= now_hour < 12:
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T06_12']
+elif 12 <= now_hour < 18:
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T12_18']
 else:
-    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T18_24'] # 今日の18-24時の降水確率を取得し、weather_nowに代入
+    weather_now = weather_json['forecasts'][0]['chanceOfRain']['T18_24']
 
-# 現在時刻の降水確率をweather_now_textに代入
+# 現在時刻の降水確率を表示
 weather_now_text = "現在の降水確率 : " + weather_now
-st.write(weather_now_text) # 現在時刻の降水確率を表示
+st.write(weather_now_text)
 
-# 今日、明日、明後日の降水確率をDadaFrameに代入
-df1 = pd.DataFrame(weather_json['forecasts'][0]['chanceOfRain'],index=["今日"]) # index名を今日という文字列に設定
-df2 = pd.DataFrame(weather_json['forecasts'][1]['chanceOfRain'],index=["明日"]) # index名を明日という文字列に設定
-df3 = pd.DataFrame(weather_json['forecasts'][2]['chanceOfRain'],index=["明後日"]) # index名を明後日という文字列に設定
+# 時間帯ラベルをマッピングする辞書
+time_labels = {
+    "T00_06": "0時～6時",
+    "T06_12": "6時～12時",
+    "T12_18": "12時～18時",
+    "T18_24": "18時～24時"
+}
 
-df = pd.concat([df1,df2,df3]) # 今日、明日、明後日の降水確率を結合して一覧にしてdfに代入
-st.dataframe(df) # 一覧にした降水確率を表示
+# 今日、明日、明後日の降水確率をDataFrameに代入
+df1 = pd.DataFrame(weather_json['forecasts'][0]['chanceOfRain'], index=["今日"])
+df2 = pd.DataFrame(weather_json['forecasts'][1]['chanceOfRain'], index=["明日"])
+df3 = pd.DataFrame(weather_json['forecasts'][2]['chanceOfRain'], index=["明後日"])
+
+# DataFrameを結合
+df = pd.concat([df1, df2, df3])
+
+# 列名を「T00_06」などから「0時～6時」などに変更
+df.rename(columns=time_labels, inplace=True)
+
+# 表示
+st.write("降水確率")
+st.dataframe(df)
+
+# 最高気温と最低気温を取得 
+def get_temperature_data(forecast):
+    max_temp = forecast['temperature']['max']['celsius'] if forecast['temperature']['max'] else "N/A"
+    min_temp = forecast['temperature']['min']['celsius'] if forecast['temperature']['min'] else "N/A"
+    return {"最高気温 (°C)": max_temp, "最低気温 (°C)": min_temp}
+
+# 今日、明日、明後日の最高気温と最低気温をDataFrameにまとめる
+temp_data = {
+    "日付": ["今日", "明日", "明後日"],
+    "最高気温 (°C)": [],
+    "最低気温 (°C)": []
+}
+
+for forecast in weather_json['forecasts']:
+    temp_info = get_temperature_data(forecast)
+    temp_data["最高気温 (°C)"].append(temp_info["最高気温 (°C)"])
+    temp_data["最低気温 (°C)"].append(temp_info["最低気温 (°C)"])
+
+temp_df = pd.DataFrame(temp_data)
+
+# インデックスを削除して再表示
+st.write("最高気温と最低気温")
+temp_df_no_index = temp_df.set_index("日付")  # "日付" 列をインデックスに設定して表示
+st.dataframe(temp_df_no_index)
 
 
 from openai import OpenAI
@@ -79,14 +115,18 @@ def run_gpt(content_text_to_gpt,content_kind_of_to_gpt,content_maxStr_to_gpt):
 
 st.write('マリーアントワネットから貴方へ')# タイトル
 
-# 書かせたい内容
-content_text_to_gpt = '取得した天気の内容を１００字以内で伝えてください。'
+# 全ての入力値をまとめて1つの変数に格納
+content_text_to_gpt =(
+    f"私はマリーアントワネットです。\n"
+    f"{city_code}の場所の{weather_now_text}と{temp_data}を基に天気情報を８０字以内で書いてください。場所の表示は不要です。\n"
+)
+
             
 # 書かせたい内容のテイストを選択肢として表示する
-content_kind_of_to_gpt = "マリーアントワネットが貴族達に話しかける感じで現代に置き換えた内容として"
+content_kind_of_to_gpt =   "マリーアントワネットが貴族達に話しかける想定で現代に置き換えた内容としてユーモアを交えて"
 
 # chatGPTに出力させる文字数
-content_maxStr_to_gpt = "100"
+content_maxStr_to_gpt = "4"
 
 output_content_text = run_gpt(content_text_to_gpt,content_kind_of_to_gpt,content_maxStr_to_gpt)
 st.write(output_content_text)
